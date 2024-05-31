e8ff478c56321ff09301afa2ab231d5b81f6d77b
Omar Dachia <omardachia@gmail.com>
2024-05-31 09:19:42 +0100

    second

--- QuestionFourteen/Properties/AssemblyInfo.cs
+++ QuestionFourteen/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("QuestionThirteen")]
+[assembly: AssemblyTitle("QuestionFourteen")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("QuestionThirteen")]
+[assembly: AssemblyProduct("QuestionFourteen")]
 [assembly: AssemblyCopyright("Copyright ©  2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("77899bf3-65e6-467a-bc49-50da4abfc03c")]
+[assembly: Guid("cecef77f-ec2a-4b34-a5b2-85d60e0f43ac")]
 
 // Version information for an assembly consists of the following four values:
 //
